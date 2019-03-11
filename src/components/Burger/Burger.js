b57a5e2681e460d6a452b53f40f81419627ab740
sguittap <sguittap@gmail.com>
2019-03-11 12:49:18 -0400

    added logic for zero ingredients

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -5,11 +5,16 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient'
 
 const burger = (props) =>{
     //transform an object of key value pairs into burger ingredients
-    const transformedIngredients = Object.keys(props.ingredients).map(ingredientkey => {
+    let transformedIngredients = Object.keys(props.ingredients).map(ingredientkey => {
         return[...Array(props.ingredients[ingredientkey])].map((_, i) => {
             return <BurgerIngredient key={ingredientkey + i} type = {ingredientkey}/>
         });
-    });
+    }).reduce((arr,el) => {
+        return arr.concat(el)
+    }, [] );
+    if (transformedIngredients.length === 0){
+        transformedIngredients = <p>please add ingredients!</p>
+    }
     
     return(
         <div className={classes.Burger}>
