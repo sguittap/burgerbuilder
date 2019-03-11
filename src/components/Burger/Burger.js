9f84200b0b6d0443e5d58b4329ed1a06e118868a
sguittap <sguittap@gmail.com>
2019-03-11 12:32:28 -0400

    dynamically added ingredients with state

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,14 +4,20 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient'
 
 
 const burger = (props) =>{
+    //transform an object of key value pairs into burger ingredients
+    const transformedIngredients = Object.keys(props.ingredients).map(ingredientkey => {
+        return[...Array(props.ingredients[ingredientkey])].map((_, i) => {
+            return <BurgerIngredient key={ingredientkey + i} type = {ingredientkey}/>
+        });
+    });
+    
     return(
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top"/>
-            <BurgerIngredient type="cheese"/>
-            <BurgerIngredient type="meat"/>
+            {transformedIngredients}
             <BurgerIngredient type="bread-bottom"/>
         </div>
     );
-}
+};
 
 export default burger;
