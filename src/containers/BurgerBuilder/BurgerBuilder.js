c9af32c5e8d951bb6d7ebde30cfd813257c3bef5
sguittap <sguittap@gmail.com>
2019-03-11 15:30:39 -0400

    added ordernow button with logic

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -19,8 +19,20 @@ class BurgerBuilder extends Component{
             cheese: 0,
             meat: 0,
         },
-        totalPrice: 4
+        totalPrice: 4,
+        purchasable: false,
     }
+
+    updatePurchaseState(ingredients){
+        const sum = Object.keys(ingredients)
+            .map(ingredientKey => {
+                return ingredients[ingredientKey];
+            })
+            .reduce((sum, el) =>{
+                return sum + el;
+            },0);
+            this.setState({purchasable:sum > 0});
+    }   
 
     addIngredientHandler= (type) =>{
         const oldCount = this.state.ingredients[type];
@@ -36,6 +48,7 @@ class BurgerBuilder extends Component{
             totalPrice: newPrice,
             ingredients: updatedIngredients,
         }); 
+        this.updatePurchaseState(updatedIngredients); 
     };
 
     removeIngredientHandler = (type) => {
@@ -55,6 +68,7 @@ class BurgerBuilder extends Component{
             totalPrice: newPrice,
             ingredients: updatedIngredients,
         }); 
+        this.updatePurchaseState(updatedIngredients);
     };
 
     render(){
@@ -71,7 +85,8 @@ class BurgerBuilder extends Component{
                     addIngredient={this.addIngredientHandler}
                     removeIngredient={this.removeIngredientHandler}
                     disabled={disabledInfo}
-                    price={this.state.totalPrice}/>
+                    price={this.state.totalPrice}
+                    purchasable={this.state.purchasable}/>
             </Aux>
         );
     }
