71b20977d665eb643bb6628c5c173a3e2d1359e7
sguittap <sguittap@gmail.com>
2019-03-13 13:22:31 -0400

    added navigation items and dummy links

--- src/components/Navigation/ToolBar/ToolBar.js
+++ src/components/Navigation/ToolBar/ToolBar.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import classes from './ToolBar.module.css'
 import Logo from '../../Logo/Logo'
+import NavigationItems from '../NavigationItems/NavigationItems'
 
 const toolbar = (props) => (
     <header className={classes.ToolBar}>
         <div>MENU</div>
         <Logo />
         <nav>
-            ...
+            <NavigationItems/>
         </nav>
     </header>
 
