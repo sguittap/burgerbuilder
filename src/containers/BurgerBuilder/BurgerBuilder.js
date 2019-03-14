8207ff6993c71c2ffe189bad4ffed8844c3f77d8
sguittap <sguittap@gmail.com>
2019-03-14 16:26:12 -0400

    remove old interceptops with willUnmount

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -127,7 +127,7 @@ class BurgerBuilder extends Component{
             disabledInfo[key] = disabledInfo[key] <= 0
         };
         let orderSummary = null;
-        let burger = this.state.error ? <p>Ingredients can't be laoded!</p> : <Spinner/>;
+        let burger = this.state.error ? <p>Ingredients can't be loaded!</p> : <Spinner/>;
 
         if(this.state.ingredients){
             burger = (
