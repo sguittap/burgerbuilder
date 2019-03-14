d1fcf27742ef24aa38d73ba40a3a42fde0f42f52
sguittap <sguittap@gmail.com>
2019-03-14 13:11:57 -0400

    added axios created firebase and post route for orders

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,7 +3,10 @@ import Aux from '../../hoc/Aux';
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
-import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary'
+import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import axios from '../../axios-orders';
+ 
+
 
 
 const INGREDIENT_PRICES = {
@@ -82,8 +85,24 @@ class BurgerBuilder extends Component{
         this.setState({purchasing: false});
     };
 
-    purchaseContinueHandler = () =>{
-        alert('You continue!');
+    purchaseContinueHandler = () => {
+        const order = {
+            ingredients: this.state.ingredients,
+            price: this.state.totalPrice,
+            customer:{
+                name: 'Pickle Rick',
+                address: {
+                    street: '123 cherryTree Ln',
+                    zip: '123456',
+                    country: 'USA',
+                },
+                email: 'squanch@squanch.com'
+            },
+            deliveryMethod: 'portal drop'
+        }
+        axios.post('/orders.json', order)
+            .then(response => console.log(response))
+            .catch(error => console.log(error));
     };
 
     render(){
