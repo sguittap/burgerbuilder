466ff766a91c953b32441a0a302c093e04983690
sguittap <sguittap@gmail.com>
2019-03-14 15:46:19 -0400

    added error modal handler

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -6,7 +6,7 @@ import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 import axios from '../../axios-orders';
 import Spinner from '../../components/UI/Spinner/Spinner'
-
+import ErrorHandler from '../../hoc/ErrorHandler/ErrorHandler'
 
 
 const INGREDIENT_PRICES = {
@@ -149,4 +149,4 @@ class BurgerBuilder extends Component{
     }
 };
 
-export default BurgerBuilder;
+export default ErrorHandler(BurgerBuilder, axios);
