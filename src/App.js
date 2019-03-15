6bf27f5a3be3a3acbe9a8721dd60958b7bbe74e3
sguittap <sguittap@gmail.com>
2019-03-15 15:36:13 -0400

    added order container

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Layout from './components/Layout/Layout'
 import BurgerBuilder  from './containers/BurgerBuilder/BurgerBuilder'
 import {Route, Switch} from 'react-router-dom'
 import Checkout from './containers/Checkout/Checkout';
+import Orders from './containers/Orders/Orders'
 
 
 class App extends Component {
@@ -11,7 +12,8 @@ class App extends Component {
       <div >
         <Layout>
           <Switch>
-            <Route path='/checkout' component={Checkout}/>  
+            <Route path='/checkout' component={Checkout}/> 
+            <Route path='/orders' component={Orders}/>  
             <Route path='/' exact component={BurgerBuilder}/>
           </Switch>
         </Layout>
