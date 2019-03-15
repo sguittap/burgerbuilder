9642a4164972c427b27714d7274c179594915cff
sguittap <sguittap@gmail.com>
2019-03-15 13:10:07 -0400

    added checkout route

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -93,29 +93,30 @@ class BurgerBuilder extends Component{
     };
 
     purchaseContinueHandler = () => {
-        this.setState({loading: true})
-        const order = {
-            ingredients: this.state.ingredients,
-            price: this.state.totalPrice,
-            customer:{
-                name: 'Pickle Rick',
-                address: {
-                    street: '123 cherryTree Ln',
-                    zip: '123456',
-                    country: 'USA',
-                },
-                email: 'squanch@squanch.com'
-            },
-            deliveryMethod: 'portal drop'
-        }
+        // this.setState({loading: true})
+        // const order = {
+        //     ingredients: this.state.ingredients,
+        //     price: this.state.totalPrice,
+        //     customer:{
+        //         name: 'Pickle Rick',
+        //         address: {
+        //             street: '123 cherryTree Ln',
+        //             zip: '123456',
+        //             country: 'USA',
+        //         },
+        //         email: 'squanch@squanch.com'
+        //     },
+        //     deliveryMethod: 'portal drop'
+        // }
 
-        axios.post('/orders.json', order)
-            .then(response => {
-                this.setState({loading:false, purchasing: false})
-            })
-            .catch(error => {
-                this.setState({loading:false, purchasing: false})
-            });
+        // axios.post('/orders.json', order)
+        //     .then(response => {
+        //         this.setState({loading:false, purchasing: false})
+        //     })
+        //     .catch(error => {
+        //         this.setState({loading:false, purchasing: false})
+        //     });
+        this.props.history.push('/checkout')
     };
 
     render(){
