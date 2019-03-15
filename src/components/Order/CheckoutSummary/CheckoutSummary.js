000542974929c5198ae26292aa44c7e33fcb163e
sguittap <sguittap@gmail.com>
2019-03-15 12:53:46 -0400

    added react router dom and path routes

--- src/components/Order/CheckoutSummary/CheckoutSummary.js
+++ src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -7,7 +7,7 @@ const checkoutSummary = (props) => {
     return(
         <div className={classes.CheckoutSummary}>
             <h1>We hope it tastes great!</h1>
-            <div style={{width: '100%', height: '300px', margin: 'auto'}}>
+            <div style={{width: '100%', margin: 'auto'}}>
                 <Burger ingredients={props.ingredients}/>
             </div>
             <Button 
