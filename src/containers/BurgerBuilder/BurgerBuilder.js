be40d462cb2dc65e77329c7728f1927ead834d6c
sguittap <sguittap@gmail.com>
2019-03-15 15:13:14 -0400

    added more functionality to checkout and post to server

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -93,36 +93,12 @@ class BurgerBuilder extends Component{
     };
 
     purchaseContinueHandler = () => {
-        // this.setState({loading: true})
-        // const order = {
-        //     ingredients: this.state.ingredients,
-        //     price: this.state.totalPrice,
-        //     customer:{
-        //         name: 'Pickle Rick',
-        //         address: {
-        //             street: '123 cherryTree Ln',
-        //             zip: '123456',
-        //             country: 'USA',
-        //         },
-        //         email: 'squanch@squanch.com'
-        //     },
-        //     deliveryMethod: 'portal drop'
-        // }
-
-        // axios.post('/orders.json', order)
-        //     .then(response => {
-        //         this.setState({loading:false, purchasing: false})
-        //     })
-        //     .catch(error => {
-        //         this.setState({loading:false, purchasing: false})
-        //     });
         const queryParams = [];
         for(let i in this.state.ingredients){
             queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
         };
+        queryParams.push('price=' + this.state.totalPrice);
         const queryString = queryParams.join('&')
-
-
         this.props.history.push({
             pathname: '/checkout',
             search: '?'+ queryString,
--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
-import Button from '../../../components/UI/Button/Button'
-import classes from './ContactData.module.css'
+import Button from '../../../components/UI/Button/Button';
+import classes from './ContactData.module.css';
+import axios from '../../../axios-orders';
+import Spinner from '../../../components/UI/Spinner/Spinner'
 
 
 class ContactData extends Component {
@@ -10,20 +12,56 @@ class ContactData extends Component {
         address: {
             street: '',
             zip: '',
-        }
+        },
+        loading: false,
     }
 
+    orderHandler = (event) => {
+        event.preventDefault();
+        this.setState({loading: true})
+        const order = {
+            ingredients: this.props.ingredients,
+            price: this.props.price,
+            customer:{
+                name: 'Pickle Rick',
+                address: {
+                    street: '123 cherryTree Ln',
+                    zip: '123456',
+                    country: 'USA',
+                },
+                email: 'squanch@squanch.com'
+            },
+            deliveryMethod: 'portal drop'
+        }
+
+        axios.post('/orders.json', order)
+            .then(response => {
+                this.setState({loading:false})
+                this.props.history.push('/');
+            })
+            .catch(error => {
+                this.setState({loading:false})
+            });
+    };
+
     render (){
-        return(
-            <div className={classes.ContactData}>
-            <h4>Enter your contact info.</h4>
+        let form = (
             <form>
                 <input className={classes.Input} type='text' name='name' placeholder='Name'/>
                 <input className={classes.Input} type='email' name='email' placeholder='Email'/>
                 <input className={classes.Input} type='text' name='street' placeholder='Street'/>
                 <input className={classes.Input} type='text' name='zip' placeholder='Zip Code'/>
-                <Button btnType='Success'>ORDER</Button>
+                <Button btnType='Success' clicked={this.orderHandler}>ORDER</Button>
             </form>
+        );
+        if(this.state.loading){
+            form = <Spinner/>
+        };
+
+        return(
+            <div className={classes.ContactData}>
+                <h4>Enter your contact info.</h4>
+                {form}
             </div>
         );
     }
