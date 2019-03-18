1994e5df779674da77cbead93ff874213f9c6f01
sguittap <sguittap@gmail.com>
2019-03-18 14:13:53 -0400

    added user input

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -54,8 +54,8 @@ class ContactData extends Component {
                 elementType: 'select',
                 elementConfig: {
                     options: [
-                        {value:'fastest', displayValue: 'Fastest'},
-                        {value:'cheapest', displayValue: 'Cheapest'},
+                        {value:'fastest', displayValue: 'Portal Drop'},
+                        {value:'cheapest', displayValue: 'Giant Sling Shot'},
                     ]
                 },
                 value: ''
@@ -82,6 +82,19 @@ class ContactData extends Component {
             });
     };
 
+    inputChangedHandler = (event, inputIdentifier) => {
+        const updatedOrderForm = {
+            ...this.state.orderForm
+        };
+        const updatedFormElement = {
+            ...updatedOrderForm[inputIdentifier]
+        };
+        updatedFormElement.value = event.target.value;
+        updatedOrderForm[inputIdentifier] = updatedFormElement;
+        this.setState({orderForm: updatedOrderForm})
+    };
+
+
     render (){
 
         const formElementsArray = [];
@@ -100,7 +113,8 @@ class ContactData extends Component {
                         key= {formElement.id}
                         elementType={formElement.config.elementType}
                         elementConfig={formElement.config.elementConfig}
-                        value={formElement.config.value }/>
+                        value={formElement.config.value}
+                        changed={(event) => this.inputChangedHandler(event, formElement.id)}/>
                 ))}
                 <Button btnType='Success' clicked={this.orderHandler}>ORDER</Button>
             </form>
