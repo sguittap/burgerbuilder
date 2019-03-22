01311cc60342ae1c4d35815fcc575600a780b936
sguittap <sguittap@gmail.com>
2019-03-22 16:16:34 -0400

    fixing checkout and contact data for redux

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -14,7 +14,6 @@ import * as actionTypes from '../../store/action'
 
 class BurgerBuilder extends Component{
     state = {
-        purchasable: false,
         purchasing: false,
         loading: false,
         error: false,
@@ -38,7 +37,7 @@ class BurgerBuilder extends Component{
             .reduce((sum, el) =>{
                 return sum + el;
             },0);
-            this.setState({purchasable:sum > 0});
+            return sum > 0;
     };   
 
     purchaseHandler = () => {
@@ -50,16 +49,7 @@ class BurgerBuilder extends Component{
     };
 
     purchaseContinueHandler = () => {
-        const queryParams = [];
-        for(let i in this.state.ingredients){
-            queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
-        };
-        queryParams.push('price=' + this.state.totalPrice);
-        const queryString = queryParams.join('&')
-        this.props.history.push({
-            pathname: '/checkout',
-            search: '?'+ queryString,
-        })
+        this.props.history.push('/checkout')
     };
 
     render(){
@@ -82,7 +72,7 @@ class BurgerBuilder extends Component{
                         removeIngredient={this.props.onIngredientRemoved}
                         disabled={disabledInfo}
                         price={this.props.price}
-                        purchasable={this.state.purchasable}
+                        purchasable={this.updatePurchaseState(this.props.ings)}
                         ordered={this.purchaseHandler}/>
                 </Aux>
             );
