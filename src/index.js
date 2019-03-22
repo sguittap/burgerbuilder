d8d528c1eefe9fc887891bbffbfe21050de7d22b
sguittap <sguittap@gmail.com>
2019-03-22 16:49:11 -0400

    added reduxDevTool

--- src/index.js
+++ src/index.js
@@ -5,10 +5,13 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import {Provider} from 'react-redux';
-import {createStore} from 'redux';
+import {createStore, compose} from 'redux';
 import reducer from './store/reducer';
 
-const store = createStore(reducer);
+
+const reduxDevTool = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+
+const store = createStore(reducer,reduxDevTool);
 
 const app = (
     <Provider store={store}>
