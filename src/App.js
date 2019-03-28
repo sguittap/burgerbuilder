c3e0ea2e4a426d757810adc750e8e0f5cb2f1d5c
sguittap <sguittap@gmail.com>
2019-03-28 12:19:00 -0400

    fixing routing and connecting issues redux

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import Layout from './components/Layout/Layout';
 import BurgerBuilder  from './containers/BurgerBuilder/BurgerBuilder';
-import {Route, Switch} from 'react-router-dom';
+import {Route, Switch, withRouter} from 'react-router-dom';
 import Checkout from './containers/Checkout/Checkout';
 import Orders from './containers/Orders/Orders';
 import Auth from './containers/Auth/Auth';
@@ -42,4 +42,4 @@ const mapDispatchToProps = dispatch => {
   }
 };
 
-export default connect(null, mapDispatchToProps)(App) ;
+export default withRouter(connect(null, mapDispatchToProps)(App));
